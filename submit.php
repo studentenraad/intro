e712e588a73fd6b2bd101f4ac98d4ce687da3ea1
Larry Boltovskoi <larry.bolt@gmail.com>
2013-09-26 21:02:33 +0200

    submit bug, bugga

--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST) && isset($_GET['type']) && $_GET['type'] == 'contact')
 {
-    @file_put_contents('.messages.txt', "{$_POST['name']} - {$_POST['email']}\r\n{$_POST['message']}\r\n-------------\r\n");
+    @file_put_contents('.messages.txt', @file_get_contents('.messages.txt')."{$_POST['name']} - {$_POST['email']}\r\n{$_POST['message']}\r\n-------------\r\n");
     header('Location: /?submitted#help');
 }
