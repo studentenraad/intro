ef32dd5b3540bb2a284c5162e89a41735e9b8237
Larry Boltovskoi <larry.bolt@gmail.com>
2013-09-26 22:51:32 +0200

    added tracking, fixed quote

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		<script src="js/init.js"></script>
 		<noscript>
 			<link rel="stylesheet" href="css/skel-noscript.css" />
-			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style.css?v3" />
 			<link rel="stylesheet" href="css/style-wide.css" />
 		</noscript>
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
@@ -273,15 +273,22 @@
 		<!-- Footer -->
 			<div id="footer">
 
-				<!-- Copyright -->
 					<div class="copyright">
 						<ul class="menu">
-							<li><a href="http://html5up.net"><i class="icon icon-asterisk" title=Yes! It is a template! But looks damn pretty anyway..."></i></a></li>
+							<li><a href="http://html5up.net"><i class="icon icon-asterisk" title="Yes! It is a template! But looks damn pretty anyway..."></i></a></li>
                             <li>“Efficiency is intelligent laziness.”</li>
 						</ul>
 					</div>
-
 			</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-44218789-2', 'groeptstudent.com');
+  ga('send', 'pageview');
+
+</script>
 	</body>
 </html>
